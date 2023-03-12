d13a4498b725da8d0b6bc76f59376356abd1efcd
Brendan Teodorescu <hockey14@live.com>
2023-03-12 15:56:31 -0400

    Established Base Spec File

---
+++ spec/employee-controller.test.ts
@@ -0,0 +1,52 @@
+import { EmployeeController } from "../src/controller/employee.controller"
+import { EmployeeResponseDto } from "../src/model/dto/response/employee.response-dto";
+import { EmployeeService } from "../src/service/employee.service";
+
+/*
+
+describe("EmployeeController", () => {
+    let testSubject: EmployeeController = null;
+    
+
+    const setup = () => {
+        const test = new EmployeeController(employeeServiceMock, employeeMapperMock);
+        test.setRequest(MockRequest);
+        test.SetResponse(mockResponse);
+        return test;
+    };
+
+    const teardown = () => {
+        employeeServiceMock = null;
+        employeMapperMock = null;
+        testSubject = null;
+    };
+
+    beforeEach(() => []);
+
+    afterEach(() => {
+        teardown();
+    });
+
+
+    describe("Infrastructure", () => {
+        it("Should create an instance.", () => {
+            testSubject = setup();
+
+            expect(testSubject).toBeTruthy();
+            expect(testSubject).toBeInstanceOf(EmployeeController);
+        });
+    });
+
+    describe("getEmployees", () => {
+        it("Should return a successful response with the EmployeeResponseDto.", async () => {
+            testSubject = setup();
+            const successMsg: string = "Successfully retrieved employees.";
+            const expected: ApiResponseDto<EmployeeResponseDto[]> = 
+
+            expect(testSubject).toBeTruthy();
+            expect(testSubject).toBeInstanceOf(EmployeeController);
+        });
+    });
+})
+*/
+
